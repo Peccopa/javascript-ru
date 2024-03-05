bbeb7bf36128e03001f5d5d6c12aa54c2ab7cf4a
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-03-05 14:08:43 +0300

    feat: add GuessTheNumber task

--- 004_Interaction/script.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-// Создайте страницу, которая спрашивает имя у пользователя и выводит его.
-let userName = prompt('Want is your name?', '');
-let userBoolean = confirm('Question?');
-alert(userAnswer + ' ' + userBoolean);
