133969728c69b0321f5dc476eb863bf29aa5cba0
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-03-05 11:08:50 +0300

    feat: add basic operators section

--- 004_Interaction/script.js
+++ 004_Interaction/script.js
@@ -1,1 +1,5 @@
 'use strict';
+// Создайте страницу, которая спрашивает имя у пользователя и выводит его.
+let userName = prompt('Want is your name?', '');
+let userBoolean = confirm('Question?');
+alert(userAnswer + ' ' + userBoolean);
