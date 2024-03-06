6067df532f22c7b35f92fa21312e8eaa06a2f6ba
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-03-06 13:02:18 +0300

    feat: add win game option

--- 01_GuessTheNumber/script.js
+++ 01_GuessTheNumber/script.js
@@ -10,9 +10,35 @@
 //   console.log(document.querySelector('.number-input').value);
 // };
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.question').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', () => {
   const guessingNumber = Number(document.querySelector('.number-input').value);
   if (!guessingNumber) {
-    document.querySelector('.guess-message').textContent = 'Input some number!';
+    document.querySelector('.guess-message').textContent = 'Введите число!';
+  } else if (guessingNumber === secretNumber) {
+    document.querySelector('.guess-message').textContent = 'Правильно!';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.question').style.width = '50rem';
+  } else if (guessingNumber > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Слишком много!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guessingNumber < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Слишком мало!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
