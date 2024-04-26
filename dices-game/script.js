e76aec0708f894eafd04412ca7a51b0f6a6e87f2
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-04-26 11:14:26 +0300

    feat: add active dice

--- dices-game/script.js
+++ dices-game/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Elements selection
+const score0Element = document.getElementById('score--0');
+const score1Element = document.getElementById('score--1');
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore = 0;
+
+// Game initial conditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
+
+// Roll the dice
+btnRoll.addEventListener('click', () => {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice${diceNumber}.png`;
+  if (diceNumber !== 1) {
+    currentScore += diceNumber;
+    current0Element.textContent = currentScore;
+  }
+});
