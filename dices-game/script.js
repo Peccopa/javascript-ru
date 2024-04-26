c2be4c6e4460b29614e9b7ca0c940caaabfc8d85
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-04-26 11:37:12 +0300

    feat: add active player

--- dices-game/script.js
+++ dices-game/script.js
@@ -9,12 +9,17 @@ const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-let currentScore = 0;
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 
 // Game initial conditions
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
+
+const totalScores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 // Roll the dice
 btnRoll.addEventListener('click', () => {
@@ -23,6 +28,14 @@ btnRoll.addEventListener('click', () => {
   diceElement.src = `dice${diceNumber}.png`;
   if (diceNumber !== 1) {
     currentScore += diceNumber;
-    current0Element.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
