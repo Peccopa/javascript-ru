6ff62ac6b44d34f72b0dcd7cea7ab079ed1c90dc
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-07-09 17:45:17 +0300

    feat: task-084 complete

---
+++ javascript-02/task-084/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// const wallet = {
+//   balance: 0,
+//   operations: [],
+//   inputFunds(reason, sum) {
+//     this.balance += sum;
+//     this.operations.push({
+//       reason,
+//       sum,
+//     });
+//     return console.log(true, this.operations);
+//   },
+//   outputFunds(reason, sum) {
+//     if (this.balance < sum) return console.log(false, this.operations);
+//     this.balance -= sum;
+//     this.operations.push({
+//       reason,
+//       sum,
+//     });
+//     return console.log(true, this.operations);
+//   },
+//   operationsCount() {
+//     return console.log(this.operations.length);
+//   },
+// };
+
+// wallet.inputFunds('Зарплата', 10000);
+// wallet.outputFunds('Оплата налогов', 15000);
+// wallet.inputFunds('Аванс', 7000);
+// wallet.outputFunds('Оплата налогов', 15000);
+// wallet.operationsCount();
+
