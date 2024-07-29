13c9d60582e7a5e83cc3750236e1ecaaa5a14eba
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-07-29 08:05:30 +0300

    refactot: add webpack

---
+++ src/index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// module
+
+import { Article } from './js/Article.js';
+import { data } from './js/Data.js';
+
+// console.log(Article);
+// console.log(data);
+
+// Page
+
+window.onload = function () {
+  console.log('Hello, worm!');
+  addTagsClickHandler();
+  if (data) {
+  }
+};
+
+const addTagsClickHandler = () => {
+  document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+    if (e.target.classList.contains('tag')) {
+      const clickedTag = e.target;
+      removeSelectedTags();
+      selectClickedTag(clickedTag);
+      if (clickedTag.innerText === 'All') {
+        showAllStrategies();
+      } else {
+        filterStrategiesBySelectedTag(clickedTag.innerText);
+      }
+    }
+  });
+};
+
+const removeSelectedTags = () => {
+  const tags = document.querySelectorAll('.strategies__tags .tag');
+  tags.forEach((tag) => {
+    tag.classList.remove('tag_selected');
+    tag.classList.add('tag_bordered');
+  });
+};
+
+const selectClickedTag = (clickedTag) => {
+  clickedTag.classList.remove('tag_bordered');
+  clickedTag.classList.add('tag_selected');
+};
+
+const showAllStrategies = () => {
+  const strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+  strategies.forEach((strategy) => {
+    strategy.classList.remove('strategy_hidden');
+  });
+};
+
+const filterStrategiesBySelectedTag = (selectedTag) => {
+  const strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+  strategies.forEach((strategy) => {
+    strategy.classList.add('strategy_hidden');
+    strategy.querySelectorAll('.tag').forEach((tag) => {
+      if (tag.innerHTML === selectedTag)
+        strategy.classList.remove('strategy_hidden');
+    });
+  });
+};
