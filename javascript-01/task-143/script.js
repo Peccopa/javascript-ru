f6ed8842fccf44d179572e2bb80ed0acdddcc206
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-09-11 13:42:41 +0300

    feat: add webpage for simply bank

--- javascript-01/task-143/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-  const h2 = document.querySelector('h2');
-  h2.style.color = 'orange';
-  document.body.addEventListener('click', (e) => {
-    e.target.style.color = 'green';
-  });
-})();
--- javascript-02/task-078/script.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// const card = '1234123412341234';
-
-// console.log('**** **** **** ' + card.slice(-4));
-// console.log(card.slice(-4).padStart(16, '*'));
