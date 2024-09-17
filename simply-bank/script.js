29edf4985e64e0290e7bbf426c31359bfbef2ae9
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-09-17 15:10:20 +0300

    feat: lazy loading img  - intersection observer api)

--- simply-bank/script.js
+++ simply-bank/script.js
@@ -129,3 +129,22 @@ allSections.forEach(section => {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// lazy loading img (Intersection Observer API)
+
+const lazyImages = document.querySelectorAll('img[data-src]');
+const loadImages = function (entries, observer) {
+  const entry = entries[0];
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', () =>
+    entry.target.classList.remove('lazy-img')
+  );
+  observer.unobserve(entry.target);
+};
+const lazyImagesObserver = new IntersectionObserver(loadImages, {
+  root: null,
+  threshold: 0.7,
+});
+lazyImages.forEach(image => lazyImagesObserver.observe(image));
+console.log(lazyImages);
