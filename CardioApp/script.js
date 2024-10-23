9e65071b3ec97a6012ebd6d720d2855c02eb3985
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-10-23 09:24:19 +0300

    init: start cardio app

---
+++ CardioApp/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--temp');
+const inputElevation = document.querySelector('.form__input--climb');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude},11z`);
+    },
+    function () {
+      alert('Невозможно получить ваше местоположение.');
+    }
+  );
+}
