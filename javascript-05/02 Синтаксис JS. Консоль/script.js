c1c8f40f936e7a29c60bed562bb13f0a8f34f9b4
Andrey Zharkikh <orbital.logistics@yandex.ru>
2025-05-19 09:57:35 +0300

    feat: add js-05-02

---
+++ javascript-05/02 Синтаксис JS. Консоль/script.js
@@ -0,0 +1,19 @@
+console.log(3 + '2');
+let a = 5;
+a = 8 + a; //13 -> a
+console.log(a);
+//alert(a);
+//out.innerHTML = '<i>Hi</i>';
+//out.innerText = '<i>Hi</i>';
+//document.getElementById('out').innerHTML = 777;
+let p;
+p = document.getElementById('out');
+p = document.querySelector('#out');
+p.innerHTML = 558885;
+console.log(p);
+let c = 'hello';
+let d = 'world';
+console.log(c + d);
+let year = 1982;
+console.log(year);
+document.querySelector('#year').innerHTML = `<i>${year}</i>`;
